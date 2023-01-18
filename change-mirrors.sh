7d5ec03d794464adac45add854c0509231992546
midoks <midoks@163.com>
2023-01-18 20:16:14 +0800

    up

--- change-mirrors.sh
+++ change-mirrors.sh
@@ -244,12 +244,59 @@ function ChooseMirrors() {
     echo "${SYSTEM_JUDGMENT} = ${SYSTEM_CENTOS} -o ${SYSTEM_JUDGMENT} = ${SYSTEM_RHEL}"
 }
 
+function DownloadScript(){
+    curl -sSLo /tmp/dev.zip https://github.com/midoks/change-linux-mirrors/archive/refs/heads/main.zip
+    cd /tmp && unzip /tmp/dev.zip
+}
+
+# 安装
+function InstallScript(){
+
+    _os=`uname`
+    echo "use system: ${_os}"
+    if [ ${_os} == "Darwin" ]; then
+        OSNAME='macos'
+    elif grep -Eq "openSUSE" /etc/*-release; then
+        OSNAME='opensuse'
+        zypper refresh
+    elif grep -Eq "FreeBSD" /etc/*-release; then
+        OSNAME='freebsd'
+    elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        OSNAME='rhel'
+        yum install -y wget zip unzip
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        OSNAME='fedora'
+        yum install -y wget zip unzip
+    elif grep -Eqi "Rocky" /etc/issue || grep -Eq "Rocky" /etc/*-release; then
+        OSNAME='rhel'
+        yum install -y wget zip unzip
+    elif grep -Eqi "AlmaLinux" /etc/issue || grep -Eq "AlmaLinux" /etc/*-release; then
+        OSNAME='rhel'
+        yum install -y wget zip unzip
+    elif grep -Eqi "Amazon Linux" /etc/issue || grep -Eq "Amazon Linux" /etc/*-release; then
+        OSNAME='amazon'
+        yum install -y wget zip unzip
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        OSNAME='debian'
+        apt update -y
+        apt install -y devscripts
+        apt install -y wget zip unzip
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        OSNAME='ubuntu'
+        apt install -y wget zip unzip
+    else
+        OSNAME='unknow'
+    fi
+    
+    bash /tmp/change-linux-mirrors-main/script/${OSNAME}.sh
+}
+
 
 function RunMain(){
 	PermissionJudgment
-    EnvJudgment
-    ChooseMirrors
-
+    DownloadScript
+    InstallScript
+    AuthorSignature
 }
 
 # 执行
---
+++ script/debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Author: midoks
+## Modified: 2023-01-18
+## License: Apache License
+## Github: https://github.com/midoks/change-linux-mirrors
+
+echo "hh"
